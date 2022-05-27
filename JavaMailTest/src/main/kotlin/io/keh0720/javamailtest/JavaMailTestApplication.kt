d148acee6e728fde544d6697432da43dbac2eaac
keh0720 <keh0720@gmail.com>
2022-05-27 18:04:27 +0900

    Add send mail test

---
+++ JavaMailTest/src/main/kotlin/io/keh0720/javamailtest/JavaMailTestApplication.kt
@@ -0,0 +1,11 @@
+package io.keh0720.javamailtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaMailTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavaMailTestApplication>(*args)
+}
